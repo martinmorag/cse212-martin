9b5fc08e4cd34879d8079144577b118a432b6fb5
martinmorag <moragamartin2@gmail.com>
2025-03-03 23:33:48 -0300

    progress

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,18 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var result = new int[select.Length];
+        var fdx = 0;
+        var sdx = 0;
+        for (var i = 0; i < select.Length; i++) {
+            if (select[i] == 1) {
+                result[i] = list1[fdx++];
+            } else {
+                result[i] = list2[sdx++];
+            }
+        }
+
+
+        return result;
     }
 }
