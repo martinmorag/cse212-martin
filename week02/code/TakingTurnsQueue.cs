8851de32c614a5104f631229a280c079e43e4ed1
martinmorag <113954312+martinmorag@users.noreply.github.com>
2025-03-08 18:24:31 -0300

    Update TakingTurnsQueue.cs

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -40,7 +40,10 @@ public class TakingTurnsQueue
         else
         {
             Person person = _people.Dequeue();
-            if (person.Turns > 1)
+            if (person.Turns < 1) {
+                _people.Enqueue(person);
+            }
+            else if (person.Turns > 1)
             {
                 person.Turns -= 1;
                 _people.Enqueue(person);
